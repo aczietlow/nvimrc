9c44fa667bb5413eaec48e9d86d0c100909c850d
Chris Zietlow <aczietlow@gmail.com>
2025-01-08 17:05:48 -0500

    Adding keymap for executing go

--- lua/aczietlow/map.lua
+++ lua/aczietlow/map.lua
@@ -5,4 +5,17 @@ vim.g.mapleader = ' '
 vim.g.maplocalleader = ' '
 
 vim.keymap.set('n', '<leader>pv', vim.cmd.Ex, { desc = 'Navigate back to project file tree' })
+
+-- Execute go file.
+vim.keymap.set('n', '<leader>gr', function()
+  local file = vim.fn.expand '%'
+  local first_line = vim.fn.getline(1)
+  if string.match(first_line, '^//') or string.match(file, '%.go$') then
+    local escaped_file = vim.fn.shellescape(file)
+    vim.cmd 'vsplit'
+    vim.cmd('terminal go run ' .. escaped_file)
+  else
+    vim.cmd "echo 'Not a Go file.'"
+  end
+end, { desc = 'Execute current Go file' })
 --vim.keymap.set('n', '<leader>pv'vim.cmd.Ex)
