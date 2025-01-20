dd92e0a90f1cc7bb36cb5c79343f5046cc8d5f76
Chris Zietlow <aczietlow@gmail.com>
2025-01-20 12:13:18 -0500

    separating the todo module config

---
+++ lua/aczietlow/plugins/todo.lua
@@ -0,0 +1,8 @@
+-- Highlight todo, notes, etc in comments
+-- TODO: Like this
+return {
+  'folke/todo-comments.nvim',
+  event = 'VimEnter',
+  dependencies = { 'nvim-lua/plenary.nvim' },
+  opts = { signs = false },
+}
