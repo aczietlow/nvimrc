fc82ced90368ef2d15b940a2947804bbce9e05f1
Chris Zietlow <aczietlow@gmail.com>
2025-07-01 16:53:22 -0400

    don't autoformat for gotempl html files

--- lua/aczietlow/plugins/autoformat.lua
+++ lua/aczietlow/plugins/autoformat.lua
@@ -32,7 +32,17 @@ return { -- Autoformat
     end,
     formatters_by_ft = {
       lua = { 'stylua' },
-      html = { 'prettier' },
+      -- html = { 'prettier' },
+      html = function(bufnr)
+        local lines = vim.api.nvim_buf_get_lines(bufnr, 0, -1, false)
+        for _, line in ipairs(lines) do
+          if line:match '{{' then
+            return {}
+          end
+        end
+        return { 'prettier' }
+      end,
+      toml = { 'taplo' },
       -- Conform can also run multiple formatters sequentially
       -- python = { "isort", "black" },
       --
